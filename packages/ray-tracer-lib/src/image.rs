65c9f7dc1f54fe71c9f09345a63d6ff2e0fa3b2e
Julien Graziano <julien.graziano@arobas-music.com>
2025-05-02 15:58:56 +0200

    refactor: add image module

---
+++ packages/ray-tracer-lib/src/image.rs
@@ -0,0 +1,122 @@
+use glam::U8Vec4;
+
+use itertools::Itertools;
+
+pub struct Image {
+    aspect_ratio: f64,
+    height: usize,
+    width: usize,
+    pixels: Vec<U8Vec4>,
+}
+
+impl Image {
+    pub fn new(
+        width: usize,
+        height: usize,
+    ) -> Self {
+        let aspect_ratio = (width as f64)/(height as f64);
+        let pixels = vec![U8Vec4::ZERO; width*height];
+
+        Self {
+            aspect_ratio,
+            width,
+            height,
+            pixels,
+        }
+    }
+
+    pub fn new_with_width_and_aspect_ratio(
+        width: usize,
+        aspect_ratio: f64,
+    ) -> Self {
+        let height = (((width as f64)/aspect_ratio) as usize).max(1);
+        Image::new(width, height)
+    }
+
+    pub fn new_with_height_and_aspect_ratio(
+        height: usize,
+        aspect_ratio: f64,
+    ) -> Self {
+        let width = (((height as f64)*aspect_ratio) as usize).max(1);
+        Image::new(width, height)
+    }
+}
+
+impl Image {
+    pub fn get_aspect_ratio(&self) -> f64 {
+        self.aspect_ratio
+    }
+
+    pub fn get_height(&self) -> usize {
+        self.height
+    }
+
+    pub fn get_width(&self) -> usize {
+        self.width
+    }
+
+    pub fn get_pixels(&self) -> &[U8Vec4] {
+        &self.pixels[..]
+    }
+
+    pub fn get_pixels_mut(&mut self) -> &mut [U8Vec4] {
+        &mut self.pixels[..]
+    }
+
+    pub fn get_pixel_at(
+        &self,
+        x: usize,
+        y: usize,
+    ) -> Option<&U8Vec4> {
+        if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
+            self.pixels.get(y*self.width + x)
+        } else {
+            None
+        }
+    }
+
+    pub fn get_pixel_at_mut(
+        &mut self,
+        x: usize,
+        y: usize,
+    ) -> Option<&mut U8Vec4> {
+        if (0..self.width).contains(&x) && (0..self.height).contains(&y) {
+            self.pixels.get_mut(y*self.width + x)
+        } else {
+            None
+        }
+    }
+
+    pub fn iter(
+        &self,
+    ) -> impl Iterator<Item = ((usize, usize), &U8Vec4)> {
+        self.pixels.iter().enumerate().map(|(index, pixel)| {
+            let x = index%self.width;
+            let y = index/self.width;
+
+            ((x, y), pixel)
+        })
+    }
+
+    pub fn iter_mut(
+        &mut self,
+    ) -> impl Iterator<Item = ((usize, usize), &mut U8Vec4)> {
+        self.pixels.iter_mut().enumerate().map(|(index, pixel)| {
+            let x = index%self.width;
+            let y = index/self.width;
+
+            ((x, y), pixel)
+        })
+    }
+
+    pub fn map<F>(
+        &mut self,
+        mut f: F
+    ) -> &mut Self where F: FnMut(usize, usize) -> U8Vec4  {
+        Itertools::cartesian_product(0..self.height, 0..self.width)
+            .for_each(|(y, x)| {
+                self.pixels[y*self.width + x] = f(x, y);
+            });
+        self
+    }
+}
--- packages/ray-tracer-lib/src/lib.rs
+++ packages/ray-tracer-lib/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod image;
 pub mod ppm;
 pub mod ray;
