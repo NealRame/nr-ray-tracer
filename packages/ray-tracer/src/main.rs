226020c6ed89c6bd7938536a265f57e29c01362b
Julien Graziano <julien.graziano@arobas-music.com>
2025-05-12 00:42:59 +0200

    refactor: update scene config

--- packages/ray-tracer/src/main.rs
+++ packages/ray-tracer/src/main.rs
@@ -28,6 +28,10 @@ use indicatif::{
     ProgressStyle,
 };
 
+use rand::distr::weighted::WeightedIndex;
+use rand::distr::Distribution;
+use rand::{self, Rng};
+
 use nr_ray_tracer_lib::prelude::*;
 
 use crate::cli::*;
@@ -144,36 +148,67 @@ fn main() {
     let (mut file, format) = cli.output.check();
 
     // Initialize world
-    let lambertian_1 = Arc::new(Lambertian::new(DVec3::new(0.259, 0.259, 0.259)));
-    let lambertian_2 = Arc::new(Lambertian::new(DVec3::new(0.878, 0.878, 0.878)));
-
-    let dielectric_1 = Arc::new(Dielectric::new(1.50));
-    let dielectric_2 = Arc::new(Dielectric::new(1.0/1.53));
-
-    // let metal_1 = Arc::new(Metal::new(DVec3::new(0.901, 0.231, 0.184), 0.05));
-    // let metal_2 = Arc::new(Metal::new(DVec3::new(0.220, 0.412, 0.620), 0.025));
-
-    let metal_1 = Arc::new(Metal::new(DVec3::new(0.901, 0.231, 0.184), 0.0));
-    let metal_2 = Arc::new(Metal::new(DVec3::new(0.220, 0.412, 0.620), 0.0));
-
-    let world = HitableList::from(vec![
-        Box::new(Sphere::new(DVec3::new( 0.0, -100.5, -1.0), 100.0, lambertian_1.clone())),
-
-        Box::new(Sphere::new(DVec3::new( 0.0,     0.0, -1.2),   0.5, dielectric_1.clone())),
-        Box::new(Sphere::new(DVec3::new( 0.0,     0.0, -1.2),   0.4, dielectric_2.clone())),
-        Box::new(Sphere::new(DVec3::new( 0.0,     0.0, -3.0),   0.5, lambertian_2.clone())),
-
-        Box::new(Sphere::new(DVec3::new(-1.0,    0.0, -1.0),   0.5, metal_1.clone())),
-        Box::new(Sphere::new(DVec3::new( 1.0,    0.0, -1.0),   0.5, metal_2.clone())),
+    let mut world = HitableList::from(vec![
+        Box::new(Sphere::new(
+            DVec3::new( 0.0, -1000.0, 0.0),
+            1000.0,
+            Arc::new(Lambertian::new(DVec3::new(0.5, 0.5, 0.5)))
+        )),
     ]);
+
+    let mut rng = rand::rng();
+    let materials = ["lambertian", "metal", "glass"];
+
+    let material_weights = [80, 15, 5];
+    let dist = WeightedIndex::new(&material_weights).unwrap();
+
+    for a in -11..=11 {
+        for b in -11..=11 {
+            let center = DVec3::new(
+                a as f64 + 0.9*rng.random_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9*rng.random_range(0.0..1.0),
+            );
+
+            match materials[dist.sample(&mut rng)] {
+                "lambertian" => {
+                    let albedo = DVec3::from_rng_ranged(&mut rng, 0.0..1.0);
+                    let material = Arc::new(Lambertian::new(albedo));
+
+                    world.add(Box::new(Sphere::new(center, 0.2, material)));
+                },
+                "metal" => {
+                    let albedo = DVec3::from_rng_ranged(&mut rng, 0.5..1.0);
+                    let fuzz = rng.random_range(0.0..0.5);
+                    let material = Arc::new(Metal::new(albedo, fuzz));
+
+                    world.add(Box::new(Sphere::new(center, 0.2, material)));
+                },
+                "glass" => {
+                    let material = Arc::new(Dielectric::new(1.5));
+
+                    world.add(Box::new(Sphere::new(center, 0.2, material)));
+                },
+                _ => unreachable!()
+            }
+        }
+    }
+
+    let material1 = Arc::new(Dielectric::new(1.5));
+    let material2 = Arc::new(Lambertian::new(DVec3::new(0.4, 0.2, 0.1)));
+    let material3 = Arc::new(Metal::new(DVec3::new(0.7, 0.6, 0.5), 0.0));
+
+    world.add(Box::new(Sphere::new(DVec3::new( 0.0, 1.0, 0.0), 1.0, material1)));
+    world.add(Box::new(Sphere::new(DVec3::new(-4.0, 1.0, 0.0), 1.0, material2)));
+    world.add(Box::new(Sphere::new(DVec3::new( 4.0, 1.0, 0.0), 1.0, material3)));
 
     // Initialize camera
     let camera = CameraBuilder::new(image_size)
         .with_vertical_field_of_view(cli.vfov*(PI/180.))
         .with_focus_dist(cli.focus_distance)
         .with_defocus_angle(cli.defocus_angle*(PI/180.))
-        .with_look_from(DVec3::new(-2.0,  2.0,  1.0))
-        .with_look_at(  DVec3::new( 0.0,  0.0, -1.0))
+        .with_look_from(DVec3::new(13.0,  2.0,  3.0))
+        .with_look_at(  DVec3::new( 0.0,  0.0,  0.0))
         .with_view_up(  DVec3::Y)
         .with_sample_per_pixels(cli.anti_aliasing)
         .build();
