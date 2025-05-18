f29de7dcc59eb5163469f94752cc2bae890e28b6
Julien Graziano <julien.graziano@arobas-music.com>
2025-05-19 01:04:05 +0200

    refactor: clean code

--- packages/ray-tracer-lib/src/materials/metal.rs
+++ packages/ray-tracer-lib/src/materials/metal.rs
@@ -6,34 +6,6 @@ use crate::objects::HitRecord;
 use crate::ray::Ray;
 use crate::vector::*;
 
-// #[derive(Clone, Copy, Debug)]
-// pub struct Metal {
-//     pub albedo: DVec3,
-//     pub fuzz: f64,
-// }
-
-// impl Metal {
-//     pub fn from_rng(rng: &mut ThreadRng) -> Self {
-//         Self {
-//             albedo: DVec3::new(
-//                 rng.random_range(0.0..=1.0),
-//                 rng.random_range(0.0..=1.0),
-//                 rng.random_range(0.0..=1.0),
-//             ),
-//             fuzz: rng.random_range(0.0..=1.0),
-//         }
-//     }
-// }
-
-// impl Default for Metal {
-//     fn default() -> Self {
-//         Self {
-//             albedo: DVec3::ONE/2.,
-//             fuzz: 0.0,
-//         }
-//     }
-// }
-
 pub(super) fn scatter(
     albedo: DVec3,
     fuzz: f64,
